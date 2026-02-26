33b3668834e5995d6ffa87f9aa97c2b83e30673f
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-02-26 10:34:28 -0300

    agregue estructura del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Bienvenido from './components/Bienvenido'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='display-1'>HOLA MUNDO</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit sequi temporibus quia provident aperiam porro officiis ducimus architecto, unde, doloremque numquam nihil repellat adipisci et blanditiis accusantium earum consequatur, atque fuga voluptas saepe quibusdam id dolorem reiciendis! Nobis tenetur sapiente, accusamus natus ut sed molestias ex illum quidem hic itaque magni. Quos eligendi necessitatibus quam doloribus aliquam, fugit corrupti fuga!</p>
+      <Bienvenido/>
     </>
   )
 }
 
 export default App
+
---
+++ src/components/Bienvenido.jsx
@@ -0,0 +1,10 @@
+const Bienvenido = () => {
+  return (
+    <div>
+      <h2>Informacion de React</h2>
+      <p>React JS es una biblioteca de JavaScript de código abierto, mantenida por Meta (Facebook) y la comunidad, diseñada para construir interfaces de usuario (UI) dinámicas y reutilizables, especialmente en aplicaciones de una sola página (SPA). Utiliza componentes y un DOM virtual para actualizar eficientemente solo las partes cambiadas de la interfaz.</p>
+    </div>
+  )
+}
+
+export default Bienvenido
